7bd8220587ac135c7675464908cddadfc10273f7
Krzysztof <zamienny06@gmail.com>
2025-02-27 17:38:25 +0100

    a counting mechanism was made
    calculate.html - added js
    views.py - added full logic
    urls.py - added summary.html (for future)

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('calcualte', views.calculate, name='calculate')
+    path('calculate', views.calculate, name='calculate'),
+    path('summary', views.summary, name='summary')
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -30,14 +30,34 @@ def homepage(request):
 def calculate(request):
 
     # var_field passes how many digits there should be in in the number
+    if request.method == 'POST':
+        time = request.POST.get('time')
+
+        if 'time' not in request.session:
+            request.session['time'] = []
+        
+        request.session['time'].append(time)
+        request.session.modified = True
+
     first_field = request.session.get('first_field', 1),
     second_field = request.session.get('second_field', 1),
     operation = request.session.get('operation', 'addition'),
-    num_of_problems = request.session.get('num_of_problems', 10),
+    num_of_problems = request.session.get('num_of_problems', 1),
+
+    # if solved_problems remember session
+    # if 'solved_problems' not in request.session:
+    #     request.session['solved_problems'] = 0
+    
+    solved_problems = request.session.get('solved_problems', 0)
+
+    if solved_problems > num_of_problems[0] - 1:
+        print('\n'*10, 'here', '\n'*10)
+        request.session['solved_problems'] = 0
+        return redirect('summary')
 
     first_num_extrema = 10 ** (first_field[0] - 1), (10 ** first_field[0]) - 1
     second_num_extrema = 10 ** (second_field[0] - 1), (10 ** second_field[0]) - 1
-
+    
     if operation[0] != 'division':
         first_num = random.randint(first_num_extrema[0], first_num_extrema[1])
         second_num = random.randint(second_num_extrema[0], second_num_extrema[1])
@@ -60,13 +80,27 @@ def calculate(request):
                 break
 
         res = first_num / second_num
+        solved_problems[0] += 1
 
-    context = {
+    solved_problems += 1
+    request.session['solved_problems'] = solved_problems
+
+
+    context = ({
         'first_num': first_num,
         'second_num': second_num,
-        'num_of_problems': num_of_problems,
         'operation': dict(Menu.math_operations)[operation[0]],
-        'res': res
-    }
+        'res': res,
+        'num_of_problems': num_of_problems,
+        'solved_problems': solved_problems,
+    })
 
     return render(request, 'pages/calculate.html', context)
+
+def summary(request):
+
+    context = {
+        'time': [t for t in request.session.get("time", [])],
+    }
+
+    return render(request, 'pages/summary.html', context)
