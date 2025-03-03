1e1976e2760c1463c0e4b52fef70e0b480897854
Krzysztof <zamienny06@gmail.com>
2025-03-03 01:19:06 +0100

    Add custom user model and update settings

--- user/models.py
+++ user/models.py
@@ -1,3 +1,57 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The user must have an email!")
+        if self.model.objects.filter(email=email).exists():
+            raise ValueError("This email is already taken!")
+        email = self.normalize_email(email)
+
+        # blocks of login validate
+        if "login" not in extra_fields or not extra_fields["login"]:
+            raise ValueError("The user must have a login!")
+        if self.model.objects.filter(login=extra_fields["login"]).exists():
+            raise ValueError("This login is already taken!")
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        # validate superuser permissions
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    login = models.CharField(max_length=50, unique=True)
+
+    number_of_sessions = models.IntegerField(default=0)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["login"]
+
+    def __str__(self):
+        return self.email
